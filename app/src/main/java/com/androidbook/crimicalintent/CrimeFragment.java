14eb6b6359c91ca4c4659d4a24d7aa5aa3cb6352
Asad Hameed <asadhameed2002@yahoo.com>
2020-05-06 01:54:38 +0200

    Add new function to change date of crime

--- app/src/main/java/com/androidbook/crimicalintent/CrimeFragment.java
+++ app/src/main/java/com/androidbook/crimicalintent/CrimeFragment.java
@@ -1,6 +1,8 @@
 package com.androidbook.crimicalintent;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.BadParcelableException;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,7 +20,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import java.util.Date;
 import java.util.UUID;
+
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
@@ -26,6 +30,7 @@ public class CrimeFragment extends Fragment {
     private Button btnDate;
     private CheckBox cBoxSolved;
     private static final String ARG_CRIME_ID="crime_id";
+    private static final int REQUEST_CODE=0;
 
     public  static  CrimeFragment newInstance(UUID crimeid){
         Bundle args = new Bundle();
@@ -37,7 +42,6 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         UUID crimeID= (UUID) getArguments().getSerializable(ARG_CRIME_ID);
                 mCrime= CrimeLab.get(getActivity()).getCrime(crimeID);
     }
@@ -66,17 +70,15 @@ public class CrimeFragment extends Fragment {
 
         btnDate= v.findViewById(R.id.crime_date);
         btnDate.setText(mCrime.getmDate().toString());
-        //btnDate.setEnabled(false);
         btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  FragmentManager fragmentManager= getFragmentManager();
-                DatePackerFragment dialog= new DatePackerFragment();
+                DatePackerFragment dialog= DatePackerFragment.newIntent(mCrime.getmDate());
+                dialog.setTargetFragment(CrimeFragment.this,REQUEST_CODE);
                 dialog.show(getFragmentManager(),"DialogDate");
             }
         });
         mTitleField.setText(mCrime.getmTitle());
-
         cBoxSolved=v.findViewById(R.id.crime_solved);
         cBoxSolved.setChecked(mCrime.ismSolved());
         cBoxSolved.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -86,5 +88,18 @@ public class CrimeFragment extends Fragment {
             }
         });
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != Activity.RESULT_OK)
+                return;
+        if(requestCode==REQUEST_CODE){
+            Date newCrimeDate=(Date) data.getSerializableExtra(DatePackerFragment.ARG_DATE);
+            mCrime.setmDate(newCrimeDate);
+            btnDate.setText(mCrime.getmDate().toString());
+
+        }
     }
 }
