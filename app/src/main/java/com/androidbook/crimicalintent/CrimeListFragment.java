507a6d01204424e1c8fe8b67966c83e5fa733ac2
Asad Hameed <asadhameed2002@yahoo.com>
2020-05-13 03:00:46 +0200

    when press back after create new crime then crime count is update

--- app/src/main/java/com/androidbook/crimicalintent/CrimeListFragment.java
+++ app/src/main/java/com/androidbook/crimicalintent/CrimeListFragment.java
@@ -31,7 +31,9 @@ public class CrimeListFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        updateUI(itemPosition);
+        //updateUI(itemPosition);
+        updateUI();
+
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -173,7 +175,13 @@ public class CrimeListFragment extends Fragment {
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter==null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        }
+        else{
+            mAdapter.notifyDataSetChanged();
+        }
+        updateSubtitle();
     }
 }
